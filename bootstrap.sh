97f554f1e451323835cea4c02dbcd3e3b84469cc
hostdp6 <d.p.hostetler@gmail.com>
2019-09-15 21:19:17 +0000

    additional bootstrap config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
-apt-get update
-apt-get install git-core
-
 DIR="/home/vagrant/src"
 REPO="https://github.com/hostdp6/flashcube.git"
+PROJECT="flashcube"
+
+apt-get update
+apt-get install git-core
+apt-get install python3-pip
+
+# create Python virtual environment
+pip3 install virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
+
+mkvirtualenv $PROJECT
+workon $PROJECT
 
 if ! [ -d "$DIR" ]; then
   mkdir $DIR
@@ -14,3 +25,15 @@ fi
 cd $DIR
 git clone $REPO
 
+# install python deps for API
+cd $DIR/flashcube/flashcube
+pip3 install -r requirements.txt
+
+# start the server
+# Apply database migrations
+echo "Applying database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+nohup python manage.py runserver &
