f7811accba6f09ef7e65b98592506a31f7b2a8f4
hostdp6 <d.p.hostetler@gmail.com>
2019-09-28 09:50:25 -0400

    moved postgress commands to root bootstrap

--- bootstrap-root.sh
+++ bootstrap-root.sh
@@ -9,3 +9,11 @@ apt-get -y update
 apt-get -y install git-core
 apt-get -y install python3-pip
 apt-get -y install postgresql postgresql-contrib libpq-dev
+
+# create pg database for django
+su postgres -c "psql -c \"CREATE DATABASE $PROJECT;\""
+su postgres -c "psql -c \"CREATE USER $PROJECT WITH PASSWORD '$PROJECT';\""
+su postgres -c "psql -c \"ALTER ROLE $PROJECT SET client_encoding TO 'utf8';\""
+su postgres -c "psql -c \"ALTER ROLE $PROJECT SET default_transaction_isolation TO 'read committed';\""
+su postgres -c "psql -c \"ALTER ROLE $PROJECT SET timezone TO 'UTC';\""
+su postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE $PROJECT TO $PROJECT;\""
