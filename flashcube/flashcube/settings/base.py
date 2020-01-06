3088cf24ec535414125d682497bb4324af6c3803
Dan  Hostetler <danhostetler@Dan-Hostetler-MacBookPro.local>
2020-01-06 11:38:35 -0500

    settings

--- flashcube/flashcube/settings/base.py
+++ flashcube/flashcube/settings/base.py
@@ -82,17 +82,29 @@ WSGI_APPLICATION = 'flashcube.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flashcube',
-        'USER': 'flashcube',
-        'PASSWORD': 'flashcube',
-        'HOST': 'localhost',
-        'PORT': '',
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'flashcube',
+            'USER': 'flashcube',
+            'PASSWORD': 'flashcube',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
