098e452101be5cc3842c43cf74e11412d12188ce
Dan  Hostetler <danhostetler@Dan-Hostetler-MacBookPro.local>
2020-01-06 12:08:46 -0500

    move storage to s3

--- flashcube/flashcube/settings/dev.py
+++ flashcube/flashcube/settings/dev.py
@@ -1,6 +1,16 @@
 from .base import *
 
+INSTALLED_APPS = INSTALLED_APPS + [
+    'storages'
+]
+
 ALLOWED_HOSTS = ['flashcube-dev.us-east-1.elasticbeanstalk.com']
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATIC_URL = '/static/'
+AWS_S3_OBJECT_PARAMETERS = {
+}
+
+AWS_STORAGE_BUCKET_NAME = 'flashcube-dev'
+AWS_S3_REGION_NAME = 'us-east-1'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
