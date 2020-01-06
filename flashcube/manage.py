b1ca7fc8f35ac69247e4d4bf7ef4fa9fc9c69183
Dan  Hostetler <danhostetler@Dan-Hostetler-MacBookPro.local>
2020-01-06 11:43:00 -0500

    env settings

--- flashcube/manage.py
+++ flashcube/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcube.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcube.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
