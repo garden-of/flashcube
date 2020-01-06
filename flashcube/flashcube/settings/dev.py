5599b9247e47f76b284929c6a2f63e9f9abfdbcf
Dan  Hostetler <danhostetler@Dan-Hostetler-MacBookPro.local>
2020-01-06 14:43:23 -0500

    logging

--- flashcube/flashcube/settings/dev.py
+++ flashcube/flashcube/settings/dev.py
@@ -9,13 +9,32 @@ ALLOWED_HOSTS = ['*']
 AWS_S3_OBJECT_PARAMETERS = {
 }
 
-AWS_STORAGE_BUCKET_NAME = 'flashcube-dev'
-AWS_S3_REGION_NAME = 'us-east-1'
 AWS_ACCESS_KEY_ID=os.environ['STATIC_S3_KEY']
-AWS_SECRET_ACCESS_KEY=os.environ['STATIC_S3_SECRET']
 AWS_DEFAULT_ACL = None
-
+AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_SECRET_ACCESS_KEY=os.environ['STATIC_S3_SECRET']
+AWS_STORAGE_BUCKET_NAME = 'flashcube-dev'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/flashcube/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
