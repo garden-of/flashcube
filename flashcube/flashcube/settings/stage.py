702230ea873a5bcb7c3d321435964a397329b495
Dan  Hostetler <danhostetler@Dan-Hostetler-MacBookPro.local>
2020-01-08 12:50:48 -0500

    fixed static file bucket name

--- flashcube/flashcube/settings/stage.py
+++ flashcube/flashcube/settings/stage.py
@@ -1,1 +1,40 @@
 from .base import *
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'storages'
+]
+
+ALLOWED_HOSTS = ['*']
+
+AWS_S3_OBJECT_PARAMETERS = {
+}
+
+AWS_ACCESS_KEY_ID=os.environ['STATIC_S3_KEY']
+AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_STORAGE_BUCKET_NAME = 'flashcube-stage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_SECRET_ACCESS_KEY=os.environ['STATIC_S3_SECRET']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/flashcube/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
