b1d5559bac019a285574a493b214321c585e51a7
Daniel Hostetler <d.p.hostetler@gmail.com>
2020-01-09 13:53:05 -0500

    fixed local settings

--- flashcube/flashcube/settings/local.py
+++ flashcube/flashcube/settings/local.py
@@ -1,14 +1,1 @@
 from .base import *
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flashcube',
-        'USER': 'flashcube',
-        'PASSWORD': 'flashcube',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
