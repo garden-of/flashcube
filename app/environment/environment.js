e971a2008909259ee01ea4a065c4c5e6871590a4
Daniel Hostetler <dphostetler@gmail.com>
2020-04-25 13:31:15 -0400

    finished build 3

--- app/environment/environment.js
+++ app/environment/environment.js
@@ -4,20 +4,20 @@ import { Platform } from 'react-native'
 const localhost = Platform.OS === 'ios' ? 'http://localhost:8000' : 'http://10.0.2.2:8000'
 
 const ENV = {
-    local: {
+    /*local: {
         apiUrl: localhost,
         fbAppId: '468157980709865',
         internalClientId: 'Qw0hi6p6osnNqxAyE5w91TjjN6Y7bXDsIu3c1jQu',
         internalClientSecret: 'ZBuIFOAfme85rTBjtWbVkHtU1c7WrDJPhwT6bYzuy4aIGCDJnex0RWfl4V2oarG5vDoltSNYWaTHMEMb2NXGP4vCmyxpgRdc06Y2QKEczDSMdEWeOxeb6z1rm10c5rHS',
         googleClientId: '498982492232-reae9rludoenv1497jcc757lm44hdspq.apps.googleusercontent.com'
-    },
-    /*local: {
+    },*/
+    local: {
         apiUrl: 'http://stage.flashcube.co/',
         fbAppId: '693470401458341',
         internalClientId: 'M1ZhmlzIU0kbBYANvjENR4Y9xzUaj21e9o2spqeu',
         internalClientSecret: 'wthwp6PqHFRjvY2b8iD7zTD6CaCxEasNehUVR0OpzUrevs8vXTxgT0RUZpVWu3JFBxGfqzzPlgqBO4ixI8J65THECDg1exKRg8iV9ahGenUC1C3DRP3idVfCr2TVXqjR',
         googleClientId: '498982492232-reae9rludoenv1497jcc757lm44hdspq.apps.googleusercontent.com'
-    },*/
+    },
     dev: {
         apiUrl: 'http://dev.flashcube.co/',
         fbAppId: '468157980709865',
