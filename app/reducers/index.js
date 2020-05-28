e106193db78ffb20761d2ec468e898b57c126b46
Daniel Hostetler <dphostetler@gmail.com>
2020-05-28 10:31:21 -0400

    fixed token refresh

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,19 +3,21 @@ import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
 import createSecureStore from "redux-persist-expo-securestore"
 
+import AsyncStorage from '@react-native-community/async-storage'
+
 import configReducer from './config'
 import userReducer from './user'
 import towerReducer from './tower'
 
 const configPersistConfig = {
     key: 'config',
-    storage: createSecureStore()
+    storage: AsyncStorage
 }
 
 const userPersistConfig = {
     key: 'user',
     whitelist: ['auth', 'profile'],
-    storage: createSecureStore()
+    storage: AsyncStorage
 }
 
 const createRootReducer = (history) => combineReducers({
